895efc499f61e0e5f3fa3d4ca27cec0aca6fb13b
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-09-29 13:14:04 +0600

    Add files via upload

---
+++ firstC.c
@@ -0,0 +1,37 @@
+// header
+#include<stdio.h>
+// main
+int main (){
+
+
+    // int a = 10;
+    // int b = 25;
+    // printf("%d\n", a + b);
+    // printf("%d\n", a - b);
+    // printf("%d\n", a * b);
+    // printf("%d\n", a / b);
+    // printf("%d\n", a % b);
+
+
+    // printf("Enter three numbers print +-,*,/,% -\n");
+    // int a, b, c ;
+    // scanf("%d %d %d", &a, &b, &c ) ;
+    // printf("%d %d %d\n", a, b, c);
+
+    // printf("%d\n", a + b + c);
+    // printf("%d\n", a - b - c);
+    // printf("%d\n", a * b * c);
+    // printf("%d\n", a / b / c);
+    // printf("%d\n", a % b % c);
+
+
+    printf("Sum of three numbers-\n");
+    int a, b, c ;
+    scanf("%d %d %d", &a, &b, &c ) ;    
+    int sum;
+    sum = a + b + c ;
+    printf("%d\n", sum);
+
+    return 0; //Exit
+
+} 
