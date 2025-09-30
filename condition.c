7d846861f7142e937d40007a1350b156c7c2a3ed
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-09-30 11:10:48 +0600

    learn condition

---
+++ condition.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main (){
+
+
+    // printf("if/elese condition\n");
+    // int tk;
+    // scanf("%d", &tk);
+    // if(tk >= 100){
+    //     printf("kacchi khabo");
+    // }
+    // else 
+    // {
+    //     printf("kiso kabo na");
+    // }
+
+    // printf("if/elese/ladder condition-\n");
+    // int tk;
+    // scanf("%d", &tk);
+    // if(tk >= 100){
+    //     printf("kacchi khabo");
+    // }
+    // else if(tk >= 50){
+    //     printf("burger khabo");
+    // }
+    // else if(tk >= 20){
+    //     printf("cha khabo");
+    // }
+    // else 
+    // {
+    //     printf("kiso kabo na");
+    // }
+
+    // nested if/else condition
+    // int tk;
+    // scanf("%d", &tk);
+    // if( tk >= 10000){
+    //     printf("Cox's Bazar jabo.\n");
+    //     if(tk >= 20000){
+    //         printf("sand beach e jabo.\n");
+    //     }
+    //     else{
+    //         printf("sea beach e jabo na.\n");}
+    // }
+    // else{
+    //     printf("Cox's Bazar jabo na.");
+    // }
+    return 0;
+}
