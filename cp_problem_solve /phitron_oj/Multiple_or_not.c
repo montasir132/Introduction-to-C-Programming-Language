a12c0e25806a5330af6cb7078b411d79662b4c88
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-10-09 15:49:12 +0600

    Update Multiple_or_not.c

--- cp_problem_solve /phitron_oj/Multiple_or_not.c
+++ cp_problem_solve /phitron_oj/Multiple_or_not.c
@@ -17,3 +17,18 @@ int main(){
     }
 }
 
+// #include<stdio.h>
+// int main()
+// {
+//     int a, b;
+//     scanf("%d %d", &a, &b );
+//     if(a % b == 0){
+//         printf("Multiples");
+//     }
+//     else if(b % a == 0){
+//         printf("Multiples");
+//     }
+//     else{
+//         printf("No Multiples");
+//     }
+// }
