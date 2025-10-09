d909e1cd4a48fce97a75682ec1009dc3f9c3ff3a
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-10-09 23:50:54 +0600

    Create  multiply.c

---
+++ cp_problem_solve /hackerrank/multiply.c
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/contests/assignment-01-a-introduction-to-c-programming-a-batch-08/challenges/multiply-16-2/problem
+
+#include<stdio.h>
+int main()
+{
+    long long int a,b;
+    scanf("%lld %lld", &a, &b);
+    printf("%lld\n", a*b);
+    return 0;
+}
