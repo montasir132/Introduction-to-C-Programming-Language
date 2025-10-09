c1f252dd9ebc206881b82d8a5ce520c2fdcb26f2
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-10-09 23:49:18 +0600

    Create Print_It.c

---
+++ cp_problem_solve /hackerrank/Print_It.c
@@ -0,0 +1,9 @@
+// https://www.hackerrank.com/contests/assignment-01-a-introduction-to-c-programming-a-batch-08/challenges/print-it-4/problem
+
+#include<stdio.h>
+int main(){
+    printf("Hello, world! I am learning C programming language. ^_^\n");
+    printf("Programming is fun and challenging. /\\/\\/\\\n");
+    printf("I want to give my 100%% dedication to learn!\tI will succeed one day.");
+    return 0;
+}
