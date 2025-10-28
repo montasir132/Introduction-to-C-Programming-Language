c52e53a328e64240dd27b52699d61b88aab2988f
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-10-28 23:21:23 +0600

    Create salami.c

---
+++ cp_problem_solve/salami.c
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/contests/practice-problem-a-introduction-to-c-programming-a-batch-08/challenges/salami/problem
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int x,y,z;
+    scanf("%d %d %d",&x,&y,&z);
+    int sum = x+y+z;
+    printf("%d",sum);
+    return 0;
+}
