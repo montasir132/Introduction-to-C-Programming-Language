a6879e8b7521a06ed87a27604f6f06a38f314519
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-10-28 23:22:18 +0600

    Create Multiply.c

---
+++ cp_problem_solve/Multiply.c
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/contests/practice-problem-a-introduction-to-c-programming-a-batch-08/challenges/multiply-16-2/problem
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    long long int a,b;
+    scanf("%lld %lld",&a,&b);
+    long long int mul=a*b;
+    printf("%lld",mul);
+    return 0;
+}
