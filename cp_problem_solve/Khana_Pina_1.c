45f67bd9ff6c96a84cd6d0bdd82a91f1e6ab3504
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-10-28 23:23:54 +0600

    Create Khana_Pina_1.c

---
+++ cp_problem_solve/Khana_Pina_1.c
@@ -0,0 +1,32 @@
+// https://www.hackerrank.com/contests/practice-problem-a-introduction-to-c-programming-a-batch-08/challenges/khana-pina/problem
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int tk;
+    scanf("%d",&tk);
+    if(tk>=1000)
+    {
+        printf("Three Kacchi");
+    }
+    else if(tk>=500)
+    {
+        printf("One Large Pizza");
+    }
+    else if(tk>=250)
+    {
+        printf("Three Small Burger");
+    }
+    else if(tk>=100)
+    {
+        printf("Three Fuchka");
+    }
+    else{
+        printf("Nothing");
+    }
+    return 0;
+}
